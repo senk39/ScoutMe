f014579514893d8f4dcc82ab41694132367452d8
Senk <paweldawidziak@gmail.com>
2020-03-14 22:55:32 +0100

    idol character icons added (still not responsive)

--- client-app/src/features/idolCharacters/dashboard/IdolCharacterList.tsx
+++ client-app/src/features/idolCharacters/dashboard/IdolCharacterList.tsx
@@ -1,36 +1,366 @@
-import React from 'react'
-import { Grid, Image } from 'semantic-ui-react'
+import React from "react";
+import { Grid, Image } from "semantic-ui-react";
 
 export const IdolCharacterList = () => {
-    return (
-        <div className="IdolListGrid">
-             <Grid columns={3} divided>
-    <Grid.Row>
-      <Grid.Column>
-        <Image src='././././assets/Chika.png' />
-      </Grid.Column>
-      <Grid.Column>
-        <Image src='/images/wireframe/media-paragraph.png' />
-      </Grid.Column>
-      <Grid.Column>
-        <Image src='/images/wireframe/media-paragraph.png' />
-      </Grid.Column>
-    </Grid.Row>
+  return (
+    <div className="IdolListGrid">
+      <Grid columns={3} divided>
+        <Grid.Row>
+          <Grid.Column>
+            <div>
+              <Image
+                className="idolImage"
+                src="././././assets/idolCharactersProfilePics/Muse/Honoka.png"
+              />{" "}
+              <div
+                className="idolImageBorder"
+                style={{ backgroundColor: "orange" }}
+              ></div>
+            </div>
+          </Grid.Column>
+          <Grid.Column>
+            <div>
+              <Image
+                className="idolImage"
+                src="././././assets/idolCharactersProfilePics/Muse/Eli.png"
+              />{" "}
+              <div
+                className="idolImageBorder"
+                style={{ backgroundColor: "red" }}
+              ></div>
+            </div>
+          </Grid.Column>
+          <Grid.Column>
+            <div>
+              <Image
+                className="idolImage"
+                src="././././assets/idolCharactersProfilePics/Muse/Hanayo.png"
+              />{" "}
+              <div
+                className="idolImageBorder"
+                style={{ backgroundColor: "yellow" }}
+              ></div>
+            </div>
+          </Grid.Column>
+        </Grid.Row>
 
-    <Grid.Row>
-      <Grid.Column>
-        <Image src='/images/wireframe/media-paragraph.png' />
-      </Grid.Column>
-      <Grid.Column>
-        <Image src='/images/wireframe/media-paragraph.png' />
-      </Grid.Column>
-      <Grid.Column>
-        <Image src='/images/wireframe/media-paragraph.png' />
-      </Grid.Column>
-    </Grid.Row>
-  </Grid>
-        </div>
-    )
-}
+        <Grid.Row>
+          <Grid.Column>
+            <div>
+              <Image
+                className="idolImage"
+                src="././././assets/idolCharactersProfilePics/Muse/Kotori.png"
+              />{" "}
+              <div
+                className="idolImageBorder"
+                style={{ backgroundColor: "green" }}
+              ></div>
+            </div>
+          </Grid.Column>
+          <Grid.Column>
+            <div>
+              <Image
+                className="idolImage"
+                src="././././assets/idolCharactersProfilePics/Muse/Maki.png"
+              />{" "}
+              <div
+                className="idolImageBorder"
+                style={{ backgroundColor: "purple" }}
+              ></div>
+            </div>
+          </Grid.Column>
+          <Grid.Column>
+            <div>
+              <Image
+                className="idolImage"
+                src="././././assets/idolCharactersProfilePics/Muse/Nico.png"
+              />
+              <div>
+                {" "}
+                <Image
+                  className="idolBorder"
+                  src="././././assets/idolCharactersProfilePics/colorBorders/lightPink.png"
+                />
+              </div>
+            </div>
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row>
+          <Grid.Column>
+            <div>
+              <Image
+                className="idolImage"
+                src="././././assets/idolCharactersProfilePics/Muse/Nozomi.png"
+              />{" "}
+              <div
+                className="idolImageBorder"
+                style={{ backgroundColor: "magenta" }}
+              ></div>
+            </div>
+          </Grid.Column>
+          <Grid.Column>
+            <div>
+              <Image
+                className="idolImage"
+                src="././././assets/idolCharactersProfilePics/Muse/Rin.png"
+              />{" "}
+              <div
+                className="idolImageBorder"
+                style={{ backgroundColor: "gray" }}
+              ></div>
+            </div>
+          </Grid.Column>
+          <Grid.Column>
+            <div>
+              <Image
+                className="idolImage"
+                src="././././assets/idolCharactersProfilePics/Muse/Umi.png"
+              />{" "}
+              <div
+                className="idolImageBorder"
+                style={{ backgroundColor: "blue" }}
+              ></div>
+            </div>
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row>
+          <Grid.Column>
+            <div>
+              <Image
+                className="idolImage"
+                src="././././assets/idolCharactersProfilePics/Aqours/Chika.png"
+              />{" "}
+              <div
+                className="idolImageBorder"
+                style={{ backgroundColor: "orange" }}
+              ></div>
+            </div>
+          </Grid.Column>
+          <Grid.Column>
+            <div>
+              <Image
+                className="idolImage"
+                src="././././assets/idolCharactersProfilePics/Aqours/Dia.png"
+              />{" "}
+              <div
+                className="idolImageBorder"
+                style={{ backgroundColor: "red" }}
+              ></div>
+            </div>
+          </Grid.Column>
+          <Grid.Column>
+            <div>
+              <Image
+                className="idolImage"
+                src="././././assets/idolCharactersProfilePics/Aqours/Hanamaru.png"
+              />{" "}
+              <div
+                className="idolImageBorder"
+                style={{ backgroundColor: "yellow" }}
+              ></div>
+            </div>
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row>
+          <Grid.Column>
+            <div>
+              <Image
+                className="idolImage"
+                src="././././assets/idolCharactersProfilePics/Aqours/Kanan.png"
+              />{" "}
+              <div
+                className="idolImageBorder"
+                style={{ backgroundColor: "green" }}
+              ></div>
+            </div>
+          </Grid.Column>
+          <Grid.Column>
+            <div>
+              <Image
+                className="idolImage"
+                src="././././assets/idolCharactersProfilePics/Aqours/Mari.png"
+              />{" "}
+              <div
+                className="idolImageBorder"
+                style={{ backgroundColor: "purple" }}
+              ></div>
+            </div>
+          </Grid.Column>
+          <Grid.Column>
+            <div>
+              <Image
+                className="idolImage"
+                src="././././assets/idolCharactersProfilePics/Aqours/Riko.png"
+              />{" "}
+              <div
+                className="idolImageBorder"
+                style={{ backgroundColor: "lightpink" }}
+              ></div>
+            </div>
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row>
+          <Grid.Column>
+            <div>
+              <Image
+                className="idolImage"
+                src="././././assets/idolCharactersProfilePics/Aqours/Ruby.png"
+              />{" "}
+              <div
+                className="idolImageBorder"
+                style={{ backgroundColor: "magenta" }}
+              ></div>
+            </div>
+          </Grid.Column>
+          <Grid.Column>
+            <div>
+              <Image
+                className="idolImage"
+                src="././././assets/idolCharactersProfilePics/Aqours/Yoshiko.png"
+              />{" "}
+              <div
+                className="idolImageBorder"
+                style={{ backgroundColor: "gray" }}
+              ></div>
+            </div>
+          </Grid.Column>
+          <Grid.Column>
+            <div>
+              <Image
+                className="idolImage"
+                src="././././assets/idolCharactersProfilePics/Aqours/You.png"
+              />{" "}
+              <div
+                className="idolImageBorder"
+                style={{ backgroundColor: "blue" }}
+              ></div>
+            </div>
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row>
+          <Grid.Column>
+            <div>
+              <Image
+                className="idolImage"
+                src="././././assets/idolCharactersProfilePics/Nijigasaki/Ai.png"
+              />{" "}
+              <div
+                className="idolImageBorder"
+                style={{ backgroundColor: "orange" }}
+              ></div>
+            </div>
+          </Grid.Column>
+          <Grid.Column>
+            <div>
+              <Image
+                className="idolImage"
+                src="././././assets/idolCharactersProfilePics/Nijigasaki/Ayumu.png"
+              />{" "}
+              <div
+                className="idolImageBorder"
+                style={{ backgroundColor: "red" }}
+              ></div>
+            </div>
+          </Grid.Column>
+          <Grid.Column>
+            <div>
+              <Image
+                className="idolImage"
+                src="././././assets/idolCharactersProfilePics/Nijigasaki/Emma.png"
+              />{" "}
+              <div
+                className="idolImageBorder"
+                style={{ backgroundColor: "yellow" }}
+              ></div>
+            </div>
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row>
+          <Grid.Column>
+            <div>
+              <Image
+                className="idolImage"
+                src="././././assets/idolCharactersProfilePics/Nijigasaki/Kanata.png"
+              />{" "}
+              <div
+                className="idolImageBorder"
+                style={{ backgroundColor: "green" }}
+              ></div>
+            </div>
+          </Grid.Column>
+          <Grid.Column>
+            <div>
+              <Image
+                className="idolImage"
+                src="././././assets/idolCharactersProfilePics/Nijigasaki/Karin.png"
+              />{" "}
+              <div
+                className="idolImageBorder"
+                style={{ backgroundColor: "purple" }}
+              ></div>
+            </div>
+          </Grid.Column>
+          <Grid.Column>
+            <div>
+              <Image
+                className="idolImage"
+                src="././././assets/idolCharactersProfilePics/Nijigasaki/Kasumi.png"
+              />{" "}
+              <div
+                className="idolImageBorder"
+                style={{ backgroundColor: "lightpink" }}
+              ></div>
+            </div>
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row>
+          <Grid.Column>
+            <div>
+              <Image
+                className="idolImage"
+                src="././././assets/idolCharactersProfilePics/Nijigasaki/Rina.png"
+              />{" "}
+              <div
+                className="idolImageBorder"
+                style={{ backgroundColor: "magenta" }}
+              ></div>
+            </div>
+          </Grid.Column>
+          <Grid.Column>
+            <div>
+              <Image
+                className="idolImage"
+                src="././././assets/idolCharactersProfilePics/Nijigasaki/Setsuna.png"
+              />{" "}
+              <div
+                className="idolImageBorder"
+                style={{ backgroundColor: "gray" }}
+              ></div>
+            </div>
+          </Grid.Column>
+          <Grid.Column>
+            <div>
+              <Image
+                className="idolImage"
+                src="././././assets/idolCharactersProfilePics/Nijigasaki/Shizuku.png"
+              />{" "}
+              <div
+                className="idolImageBorder"
+                style={{ backgroundColor: "blue" }}
+              ></div>
+            </div>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </div>
+  );
+};
 
 export default IdolCharacterList;
